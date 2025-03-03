b0bbd111ce2067815be37fa76979e949687a998c
tordbm <tordbm@icloud.com>
2025-03-03 12:41:50 +0100

    add proper question type to variable

--- src/layouts/PageLayout.vue
+++ src/layouts/PageLayout.vue
@@ -53,6 +53,7 @@ import { goToNextRoute, goToPrevRoute } from '../utils/utils'
 import { useMainStore } from '../store/mainStore'
 import { questions, AnswerOptions } from '../utils/questionaire'
 import { submitUserStudy } from '../api/api'
+import type { Question } from '../utils/types'
 
 const preventOnRoutes = ['select-art', 'recommend-art']
 
@@ -126,12 +127,12 @@ export default {
     async submit() {
       this.next()
       this.store.loading = true
-      const questionaire = Object.entries(this.store.selectedAnswers).map(
-        ([question_id, response]) => ({
-          question_id,
-          response: response as AnswerOptions,
-        })
-      )
+      const questionaire: Question[] = Object.entries(
+        this.store.selectedAnswers
+      ).map(([question_id, response]) => ({
+        question_id,
+        response: response as AnswerOptions,
+      }))
       const response = await submitUserStudy(questionaire)
       this.store.recieptId = response.id
       this.store.loading = false
